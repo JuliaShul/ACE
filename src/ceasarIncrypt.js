0eb77554dad9c60f4decd202655c9db60ab1861e
Julia <51923994+JuliaShul@users.noreply.github.com>
2019-07-25 00:02:23 +0700

    Decode fix (#2)
    * стабилизирован код

--- src/ceasarIncrypt.js
+++ src/ceasarIncrypt.js
@@ -1,74 +1,62 @@
-// @TODO Напиши, что делает функция и аргументы.
-// Напиши какой тип данных должен быть у аргументов для этого почитай
-// https://ru.wikipedia.org/wiki/JSDoc
-// Переделай аргументы на объект, пусть нужные значени будут свойствами объекта:
-// например: params = { arg1: bla, arg2: bla }
-// заодно почтай про https://learn.javascript.ru/destructuring
-function decode(cypher, key) {
-    var arrayWords = cypher.split('').map((item) => item.charCodeAt());
-    key.split('');
-
-    // @TODO Перепиши на Reduce.
-    for (var i = 0; i < arrayWords.length; i++) {
-        arrayWords[i] += Number(key[i]);
-        if (arrayWords[i] > 90) {
-            arrayWords[i] -= 26;
-        }
-    }
-    // @TODO Перепиши на Reduce.
-    var readyWord = '';
-    for (var i = 0; i < arrayWords.length; i++) {
-        readyWord += String.fromCharCode(arrayWords[i]);
-    }
-    
-    return readyWord;
+/**
+ * Расшифровывает значения в объекте.
+ * @param { key, cypher } 
+ * key (string) - ключ для расшифровки содержит числа.
+ * cypher (string) - зашифрованное предложение, содержит буквы.
+ */
+function decode({ key, cypher }) {
+    const keyArray = key.split('');
+    return cypher
+        .split('')
+        .reduce((acc, item, i) => {
+            acc.push(String.fromCharCode(item.charCodeAt() - keyArray[i]));
+            return acc;
+        }, [])
+        .join('');
 }
 
-// @TODO Напиши, что делает функция и аргументы.
-// Напиши какой тип данных должен быть у аргументов для этого почитай
-// https://ru.wikipedia.org/wiki/JSDoc
-// Передалый возвращаемое значние на объект
-// например: answer = { key1: bla, key2: bla }
+/**
+ * Шифрует строку.
+ * Возвращает объект со свойством(зашифрованная строка),значением свойства(массив-ключ для расшифровки)
+ * @param {string} words
+ */
 function encode(words) {
-    words.split('').map((item) => item.charCodeAt());
-    
-    var numbers = randomNumberString();
-
-    // @TODO Перепиши на Reduce.
-    for (let i = 0; i < arrayWords.length; i++) {
-        arrayWords[i] += numbers[i];
-        if (arrayWords[i] > 90) {
-            arrayWords[i] -= 26;
-        }
-    }
-    // @TODO Перепиши на Reduce.
-    var readyWord = '';
-    for (var i = 0; i < arrayWords.length; i++) {
-        readyWord += String.fromCharCode(arrayWords[i]);
-    }
-
-    return readyWord;
+    const numbers = randomNumberArray(words.length);
+    const arrayWords = words
+        .split('')
+        .reduce((acc, item, i) => {
+            acc.push(String.fromCharCode(item.charCodeAt() + numbers[i]));
+            return acc;
+        }, [])
+        .join('')
+    return {
+        key: numbers.join(''),
+        cypher: arrayWords,
+    };
 }
 
-// @TODO Напиши, что делает функция и аргументы.
-// Напиши какой тип данных должен быть у аргументов для этого почитай
-// https://ru.wikipedia.org/wiki/JSDoc
+/**
+ * Cоздает рандомные значения от 1 до 9.
+ * @param {number} min - минимальное значение.
+ * @param {number} max - максимальное значение.
+ */
 function random(min, max) {
     return Math.floor(Math.random() * (max - min)) + min;
 }
-
-// @TODO Напиши, что делает функция и аргументы.
-// Напиши какой тип данных должен быть у аргументов для этого почитай
-// https://ru.wikipedia.org/wiki/JSDoc
-function randomNumberString(length) {
+/**
+ * Принимает длину массива, которую необходимо создать.
+ * Возвращает новый массив с рандомными числами
+ * @param {number} length 
+ */
+function randomNumberArray(length) {
     const randomNumbersPool = [];
     for (let i = 0; i < length; i++) {
         randomNumbersPool.push(random(1, 9));
     }
-    return randomNumbersPool
+    return randomNumbersPool;
 }
 
 module.exports = {
     decode,
-    decode,
+    encode,
 }
